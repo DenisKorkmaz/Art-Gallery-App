382be8618abffcc5266817383e080d344d7fecd3
Donatella Crisante <dona.crisante@zoho.com>
2023-06-22 12:26:13 +0200

    task2

--- components/ArtPiecePreview/artPiecePreview.js
+++ components/ArtPiecePreview/artPiecePreview.js
@@ -1,9 +1,9 @@
-
 export default function ArtPiecePreview({ image, title, artist }) {
-    return (<>
-    <h2>{title}</h2>
-  <img src={image}></img>
-  <p>{artist}</p>
-  </>
-    );
-  }
+  return (
+    <>
+      <h2>{title}</h2>
+      <img src={image} style={{ width: "100px" }}></img>
+      <p>{artist}</p>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
-import useSWR from 'swr';
-import ArtPieces from '@/components/ArtPieces/artPieces';
- 
+import useSWR from "swr";
+import ArtPieces from "@/components/ArtPieces/artPieces";
+import Spotlight from "../components/Spotlight/Spotlight";
 
 export default function HomePage() {
-  
- const {data} = useSWR(`https://example-apis.vercel.app/api/art`, (URL) => fetch(URL).then((res) => res.json())); 
+  const { data } = useSWR(`https://example-apis.vercel.app/api/art`, (URL) =>
+    fetch(URL).then((res) => res.json())
+  );
   console.log(data);
-  if(!data){
+  if (!data) {
     return null;
   }
+
+  const randomPicture = data[Math.floor(Math.random() * data.length)];
   return (
     <div>
-      <ArtPieces pieces={data}/>
+      <Spotlight
+        image={randomPicture.imageSource}
+        artist={randomPicture.artist}
+      />
+      <ArtPieces pieces={data} />
     </div>
-  ); 
-}  
+  );
+}
