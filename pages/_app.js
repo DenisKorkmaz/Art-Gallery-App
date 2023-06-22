721e59622ce51ec3723c7ec43afbeb4767f41f03
Donatella Crisante <dona.crisante@zoho.com>
2023-06-22 12:45:59 +0200

    start task3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
 
 export default function App({ Component, pageProps }) {
+
+  const { data } = useSWR(`https://example-apis.vercel.app/api/art`, (URL) =>
+    fetch(URL).then((res) => res.json())
+  );
+  if (!data) {
+    return null;
+  }
+  console.log(data);
+
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+      <Spotlight
+        image={randomPicture.imageSource}
+        artist={randomPicture.artist}
+      />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,15 @@
-import useSWR from "swr";
-import ArtPieces from "@/components/ArtPieces/artPieces";
 import Spotlight from "../components/Spotlight/Spotlight";
 
-export default function HomePage() {
-  const { data } = useSWR(`https://example-apis.vercel.app/api/art`, (URL) =>
-    fetch(URL).then((res) => res.json())
-  );
-  console.log(data);
-  if (!data) {
-    return null;
-  }
-
+export default function SpotlightPage() {
+  
   const randomPicture = data[Math.floor(Math.random() * data.length)];
+
   return (
     <div>
       <Spotlight
         image={randomPicture.imageSource}
         artist={randomPicture.artist}
       />
-      <ArtPieces pieces={data} />
     </div>
   );
 }
