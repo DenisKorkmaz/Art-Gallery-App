4aeebc64ff130cc12632e357c5355cb347378ea9
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-06-24 16:28:32 +0200

    nav center

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,16 +1,31 @@
 import Link from "next/link";
-
-
 import { useRouter } from "next/router";
+import styled from "styled-components";
+
+const NavLink = styled.div`
+  a:link {
+    text-decoration: none; 
+    padding: 15px;
+  }
+  
+  width: 100%;
+  border: solid 0.5px grey;
+  display: flex;
+  justify-content: space-around;
+  margin: 20px 0 20px o;
+  grid: 5px;
+`;
 
 export default function Navigation() {
-
-    const router = useRouter();
-    return (
-        <nav>
-            <Link href="/">Spotlight</Link>
-            <Link href="/art-pieces">Art Pieces</Link>
-        </nav>
-    )
-
+  const router = useRouter();
+  return (
+    <>
+      <nav>
+        <NavLink>
+          <Link href="/">Spotlight</Link>
+          <Link href="/art-pieces">Art Pieces</Link>
+        </NavLink>
+      </nav>
+    </>
+  );
 }
